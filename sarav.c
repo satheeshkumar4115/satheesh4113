c13accbe23b34fd59a7a9651e7558d549fcd4827
satheeshkumar4115 <35728157+satheeshkumar4115@users.noreply.github.com>
2018-02-01 16:21:55 +0530

    Create sarav.c

---
+++ sarav.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the no");
+    scanf("%d",&a);
+    if((a%2)==0)
+    printf("th eno is even");
+    else
+    printf("the no is odd");
+    
+}
