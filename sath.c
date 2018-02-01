f1dc26adc36af420e1ab831623b4e11da6cadbe9
satheeshkumar4115 <35728157+satheeshkumar4115@users.noreply.github.com>
2018-02-01 15:44:54 +0530

    Create sath.c

---
+++ sath.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int a;
+    printf("enter the no:");
+    scanf("%d",&a);
+    if(a>0)
+
+        printf("the no is positive");
+        else if(a==0)
+        printf("the no is zero");
+    
+    else
+    
+        printf("the no is negative");
+
+}
